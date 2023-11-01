7ee5b700e3f1c6619d89012af1fad7feab3f984c
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-11-01 09:10:44 +0000

    security header added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
+  images: {
+    domains: ["img.youtube.com"],
+  },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
