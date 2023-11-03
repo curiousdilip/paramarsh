158c15e47726b41db391e8b25d3344c680d8985f
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-11-03 20:26:51 +0000

    Update Footer.jsx and global.css

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -15,9 +15,34 @@ const Footer = () => {
                   including strategic consulting, creative design, technology
                   integration and implementation.
                 </p>
+
                 <a href="mailto:sales@paramarsh.com" className="know-more">
                   Know More
                 </a>
+
+                <div className="social-media">
+                  <a
+                    href="https://www.linkedin.com/company/paramarsh-informatics-pvt-ltd./"
+                    target="_blank"
+                    aria-label="Linkedin"
+                  >
+                    <i className="bi bi-linkedin"></i>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/ParamarshInformaticsPvtLimited/"
+                    target="_blank"
+                    aria-label="Facebook"
+                  >
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                  <a
+                    href="https://www.youtube.com/channel/UCKSc4vVNdNzF20vxhNtwwRg"
+                    target="_blank"
+                    aria-label="Youtube"
+                  >
+                    <i className="bi bi-youtube"></i>
+                  </a>
+                </div>
               </div>
             </div>
             <div className="col-md-6">
@@ -52,29 +77,11 @@ const Footer = () => {
               © 2023 Paramarsh Informatics Pvt. Ltd. | All rights reserved.
             </div>
             <div className="col-md-6">
-              <div className="social-media">
-                <a
-                  href="https://www.linkedin.com/company/paramarsh-informatics-pvt-ltd./"
-                  target="_blank"
-                  aria-label="Linkedin"
-                >
-                  <i className="bi bi-linkedin"></i>
-                </a>
-                <a
-                  href="https://www.facebook.com/ParamarshInformaticsPvtLimited/"
-                  target="_blank"
-                  aria-label="Facebook"
-                >
-                  <i className="bi bi-facebook"></i>
-                </a>
-                <a
-                  href="https://www.youtube.com/channel/UCKSc4vVNdNzF20vxhNtwwRg"
-                  target="_blank"
-                  aria-label="Youtube"
-                >
-                  <i className="bi bi-youtube"></i>
-                </a>
-              </div>
+              Developed by{" "}
+              <a href="https://www.dilipmaurya.in/" target="_blank">
+                Dilip Kumar Maurya
+              </a>
+              .
             </div>
           </div>
         </div>
